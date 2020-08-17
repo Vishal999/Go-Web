e847db0047a2e108505b84c404aa022aa450f130
Vishal Hasija <vishal.hasija@philips.com>
2020-08-17 22:02:04 +0530

    Added structs - semester, year, course

--- 01_Template/01_Semester/main.go
+++ 01_Template/01_Semester/main.go
@@ -1,5 +1,23 @@
 package main
 
+type course struct {
+	Name   string
+	Number string
+	Units  string
+}
+
+type semester struct {
+	Term    string
+	Courses []course
+}
+
+type year struct {
+	AcademicYear string
+	Fall         semester
+	Spring       semester
+	Summer       semester
+}
+
 func main() {
 
 }
